3eb670bd5d221b2d16af352ee6d2896350e55396
SURENDRA-BABU-VUNNAM <83397060+SURENDRA-BABU-VUNNAM@users.noreply.github.com>
2024-05-12 16:56:51 +0000

    stack and heap

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -38,3 +38,12 @@ const myFunction = function(){
 console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// stack (used for primitive datatype) heap (non-primitive)
+
+// when ever stack memory is used whatever variable is used it's copy is given
+
+// when ever heap memory is used it is given to you directly and what ever 
+// changes you make it will effect/change original
