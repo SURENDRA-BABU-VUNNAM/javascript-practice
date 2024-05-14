daf27aa4d75d209d89c720a3c28052fac5aa7b7f
SURENDRA-BABU-VUNNAM <83397060+SURENDRA-BABU-VUNNAM@users.noreply.github.com>
2024-05-14 14:12:03 +0000

    math and number

---
+++ 01_basics/07_numbers_and_math.js
@@ -0,0 +1,37 @@
+// const score = 400;
+// console.log(score);
+
+const balance = new Number(400);
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(3));
+
+const rate = 123.09584325;
+// console.log(rate.toPrecision(5));
+
+const commas = 10000000000;
+// console.log(commas.toLocaleString("en-In"));
+
+// ====================>math<=================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(123.454));
+// console.log(Math.ceil(123.466));
+// console.log(Math.floor(123.888));
+// console.log(Math.max(2,3,7,100));
+// console.log(Math.min(2,3,7,100));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random()* (max-min+1))+min)
+
+
+
+
