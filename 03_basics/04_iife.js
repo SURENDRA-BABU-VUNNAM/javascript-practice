b8b5aed15fb9a63565306a7783393cb5606d74dd
SURENDRA-BABU-VUNNAM <83397060+SURENDRA-BABU-VUNNAM@users.noreply.github.com>
2024-05-26 16:41:59 +0000

    iffe

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expressions
+
+(function chocolate(){
+    console.log(`DB CONNECT`);
+})();
+
+((name)=>{
+    console.log(`DB CONNECTN 2 ${name}`);
+} )(`surendra`)
