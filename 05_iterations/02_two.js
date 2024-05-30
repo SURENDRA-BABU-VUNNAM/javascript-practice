689166ea9dfc099d1f4bf61b00cf42271ae84380
SURENDRA-BABU-VUNNAM <83397060+SURENDRA-BABU-VUNNAM@users.noreply.github.com>
2024-05-30 10:41:39 +0000

    while and do-while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,20 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`this is :${index}`);
+//     index = index+2
+// }
+
+// let my_array = [`flash`,`batman`,`superman`,`green lantern`]
+// let index = 0
+// while (index < my_array.length) {
+//     console.log(my_array[index]);
+//     index = index+1
+// }
+
+let score = 11
+
+do {
+    console.log(`score is : ${score}`);
+    score = score+1;
+} while (score <=10 );
